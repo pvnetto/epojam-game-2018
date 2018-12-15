5500a46000e44fc726701eebb9b9ea26334986f9
Will <will.lucen4@gmail.com>
2018-12-15 15:53:02 -0300

    Npc death and patrol behaviour implemented

---
+++ CrownGame/Assets/Scripts/Npc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour, IHittable
+{
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+    private float direction;
+
+    [SerializeField] private float movementRange;
+    [SerializeField] private float speed;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        targetPosition = new Vector3(initialPosition.x + movementRange, initialPosition.y, initialPosition.z);
+        direction = 1;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position == targetPosition)
+        {
+            direction *= -1;
+            targetPosition = new Vector3(initialPosition.x + (direction * movementRange), initialPosition.y, initialPosition.z);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    public void Hit(GameObject attacker, ref HitRecord hitRecord, Vector2 knockbackForce)
+    {
+        Destroy(gameObject);
+    }
+
+    public bool isAlly(int id)
+    {
+        return false;
+    }
+}
